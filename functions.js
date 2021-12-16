f2d5da278589e9d51b3696d5b3a45bfde17f007e
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-16 09:24:02 +0530

    function

---
+++ functions.js
@@ -0,0 +1,42 @@
+function greet()
+{
+    console.log('hai');
+}
+greet();
+
+
+function square(num){
+return num*num;
+}
+let num = square(5);
+console.log(num);
+
+function add(a,b){
+    return a+b;
+}
+let a = 10;
+b = 12;
+console.log(a+b)
+
+function add(num){
+    return num = num1+num2;
+}let num1 = 3;
+num2 = 7;
+console.log(num1+num2);
+
+
+
+function subtract(x,y){
+    return x-y;
+}
+let x=15;
+y=5;
+console.log(x-y)
+
+
+// function multipication(a,b){
+//     return a*b;
+// }
+// let a=5;
+// b=10;
+// console.log(a*b)
